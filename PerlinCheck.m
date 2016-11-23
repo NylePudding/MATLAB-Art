7fa12a2226c7739e931e6fab0fd98abf669cf61a
NylePudding <eeu41f@bangor.ac.uk>
2016-11-23 16:58:56 +0000

    Stuff and things

--- PerlinCheck.m
+++ PerlinCheck.m
@@ -2,40 +2,42 @@ clc
 close all
 clear
 
+interval = 1;
+upper_bound = 2;
+
 % % perlin_noise(x,g,ax,ay)
 
-[ax,ay] = meshgrid(0:10,0:10); % grid
-% [xx,yy] = meshgrid(1.005:0.025:4,1.005:0.025:4); % test data
-% 
+[ax,ay] = meshgrid(0:interval:upper_bound,0:interval:upper_bound); % grid
+[xx,yy] = meshgrid(0:0.001:upper_bound,0:00.1:upper_bound); % test data
+
 ax = ax(:); ay = ay(:); 
-% 
-% g = randn(numel(ax),2); g = g./repmat(sqrt(sum(g.^2,2)),1,2)*2;
-% for i = 1:numel(xx)
-%     f(i) = perlin_noise([xx(i) yy(i)],g,ax,ay);
-% end
-% figure, surf(xx,yy,reshape(f,size(xx)))
-% shading interp
-% rotate3d
 
-
-no_points = 1000
-
-
-points = linspace(0,1,no_points);
-points = [points;linspace(0,0,no_points)];
-points = points';
-
-g = randn(numel(points),2);
-
-for i = 1:numel(points(:,1))
-    f(i) = perlin_noise(points(i,:),g,ax,ay);
-    
+g = randn(numel(ax),2); g = g./repmat(sqrt(sum(g.^2,2)),1,2)*2;
+for i = 1:numel(xx)
+    f(i) = perlin_noise([xx(i) yy(i)],g,ax,ay);
 end
+figure, surf(xx,yy,reshape(f,size(xx)))
+shading interp
+rotate3d
 
 
-f
-
-plot(f)
+% no_points = 10000;
+% 
+% 
+% points = linspace(0,upper_bound,no_points);
+% points = [points;linspace(0,upper_bound,no_points)];
+% points = points';
+% 
+% g = randn(numel(ax),2);
+% 
+% for i = 1:numel(points(:,1))
+%     f(i) = perlin_noise(points(i,:),g,ax,ay);
+% end
+% 
+% 
+% f;
+% 
+% plot(f)
 
 
 
